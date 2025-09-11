011838942cfa5fb932683fe6c71d77aeadb71d57
Aksh <aksh.patel@live.com>
2025-09-11 15:55:27 +0530

    Updated import

--- ALapp/src/App.jsx
+++ ALapp/src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AppNavbar from './AppNavbar';
-import Dashboard from './pages/ClientPage/page';
+import Client from './pages/ClientPage/page';
 import { PrimeReactProvider } from 'primereact/api';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
